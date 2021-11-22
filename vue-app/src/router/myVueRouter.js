cf4ac311a19f7b0dc6f80ea2c028b2753b826ae5
scaler <zengkaiwang@yeah.net>
2021-11-23 00:47:45 +0800

    feat: 完善install方法-给每个组件添加$route和$router

--- vue-app/src/router/myVueRouter.js
+++ vue-app/src/router/myVueRouter.js
@@ -5,6 +5,24 @@ class VueRouter {
 VueRouter.install = function(Vue) {
   console.log('Vue11', Vue)
 
+  Vue.mixin({
+    beforeCreated() {
+      if(this.$options && this.$options.router) { // 如果是根组件
+        this._root = this // //把当前实例挂载到_root上
+        this._router = this.$options.router
+      } else { // 如果是子组件
+        this._root = this.$parent && this.$parent._root
+      }
+
+      // 数据代理
+      Object.defineProperty(this, '$router', {
+        get() {
+          return this._root._router
+        }
+      })
+    }
+  })
+
   // 然后再通过传进来的Vue创建两个组件router-link和router-view
   Vue.component('router-link', {
     render(h) {

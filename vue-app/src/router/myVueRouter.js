f44a09fc37913b70fff7a94143c07af384a8d03e
scaler <zengkaiwang@yeah.net>
2021-11-23 23:31:45 +0800

    feat: 全局注册router-view组件时使用渲染函数render动态渲染当前路由对应的组件&使用Vue提供的API：defineReactive 监听当前路由的变化

--- vue-app/src/router/myVueRouter.js
+++ vue-app/src/router/myVueRouter.js
@@ -1,4 +1,3 @@
-
 class HistoryRoute {
   constructor() {
     this.current = null
@@ -16,9 +15,9 @@ class VueRouter {
   }
 
   init() {
-    if(this.mode == 'hash') {
+    if (this.mode == 'hash') {
       // 先判断用户打开时,有没有hash值,没有则跳转到#
-      location.hash ? '' : location.hash = '/'
+      location.hash ? '' : (location.hash = '/')
       window.addEventListener('load', () => {
         this.history.current = location.hash.slice(1)
       })
@@ -26,7 +25,7 @@ class VueRouter {
         this.history.current = location.hash.slice(1)
       })
     } else {
-      location.pathname ? '' : location.pathname = '/'
+      location.pathname ? '' : (location.pathname = '/')
       window.addEventListener('load', () => {
         this.history.current = location.pathname
       })
@@ -45,15 +44,19 @@ class VueRouter {
   }
 }
 
-VueRouter.install = function(Vue) {
-  console.log('Vue11', Vue)
-
+VueRouter.install = function (Vue) {
   Vue.mixin({
-    beforeCreated() {
-      if(this.$options && this.$options.router) { // 如果是根组件
+    beforeCreate() {
+      if (this.$options && this.$options.router) {
+        // 如果是根组件
         this._root = this // //把当前实例挂载到_root上
         this._router = this.$options.router
-      } else { // 如果是子组件
+
+        // 利用了Vue提供的API：defineReactive，使得this._router.history对象得到监听。
+        // Vue.util.defineReactive(this._router.history,"current",this._router.history.current)
+        Vue.util.defineReactive(this, 'ccc', this._router.history)
+      } else {
+        // 如果是子组件
         this._root = this.$parent && this.$parent._root
       }
 
@@ -61,29 +64,32 @@ VueRouter.install = function(Vue) {
       Object.defineProperty(this, '$router', {
         get() {
           return this._root._router
-        }
+        },
       })
 
       // 实现$route, 达到每个组件中能使用this.$route
       Object.defineProperty(this, '$route', {
         get() {
           return this._root._router.history.current
-        }
+        },
       })
-    }
+    },
   })
 
   // 然后再通过传进来的Vue创建两个组件router-link和router-view
   Vue.component('router-link', {
     render(h) {
       return h('a', {}, '首页')
-    }
+    },
   })
 
+  // 完善router-view组件
   Vue.component('router-view', {
     render(h) {
-      return h('h1', {}, '首页视图')
-    }
+      const current = this._self._root._router.history.current
+      const routeMap = this._self._root._router.routesMap
+      return h(routeMap[current])
+    },
   })
 }
 
